1bf692354ccaa9e5c51c5bcdaf0834452961dd3f
Apre joshua <aprejoshua@gmail.com>
2023-02-17 20:01:56 -0800

    ah

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,15 +15,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("%d is positive\n", n)
+printf("%d is positive\n", n);
 }
 else if (n == 0)
 {
-printf("%d is zero\n", n)
+printf("%d is zero\n", n);
 }
 else
 {
-printf("% is negative", n)
+printf("% is negative", n);
 }
 return (0);
 }
