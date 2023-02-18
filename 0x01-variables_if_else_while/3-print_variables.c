24c2345b049ec9dc5ed3f67414b72cac330139e9
Apre joshua <aprejoshua@gmail.com>
2023-02-17 19:45:45 -0800

    both uppercase and lowercase

---
+++ 0x01-variables_if_else_while/3-print_variables.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Displays the alphabet in lowercase and uppercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c;
+
+/* Iterate through the alphabet from 'a' to 'z'*/
+for (c = 'a'; c <= 'z'; c++)
+{
+/* Print each character using putchar*/
+putchar(c);
+}
+
+/* Iterate through the alphabet from 'A' to 'Z'*/
+for (c = 'A'; c <= 'Z'; c++)
+{
+/*Print each character using putchar*/
+putchar(c);
+}
+
+/*Return 0 to indicate success*/
+return (0);
+}
