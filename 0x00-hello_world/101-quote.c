695159d7db0d134fd91fcfa555209e9d230eff84
Apre joshua <aprejoshua@gmail.com>
2023-02-28 20:59:56 -0800

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <unistd.h>
+
 /*                                   
 ** main: entry point
 *a program that prints a text
 *return: 1                          
 */                                   
-int main(void)                       
+int main(void)
 {                                    
-write(2, "and that piece of art is useful\n"" - Dora Korpar, 2015-10-19\n", 59);
+write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,9 @@
+_putchar(' ');
+        } else {
+            putchar(' ');
+            putchar('0' + result / 10);
+            putchar('0' + result % 10);
+            putchar(' ');
+        }
+    }
+}
