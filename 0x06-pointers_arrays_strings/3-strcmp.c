69f33bbc2b3e6326b467ea5aefdbc1529892c036
Apre joshua <aprejoshua@gmail.com>
2023-03-02 17:40:51 -0800

    reverse

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,7 +5,7 @@
  * @s1 : string to be compared
  * @s2 : string to be compared
  *
- * Return:0 if s1 == s2, -1 if s1 < s2, 1 if s1 > s2
+ * Return: s1[i] -s2[i]
 */
 int _strcmp(char *s1, char *s2)
 {
@@ -16,17 +16,5 @@ while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
 {
 i++;
 }
-
-if (s1[i] == s2[i])
-{
 return (0);
-}
-else if (s1[i] < s2[i])
-{
-return (-1);
-}
-else
-{
-return (1);
-}
 }
