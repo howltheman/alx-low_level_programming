1a8ecfc70db243367f6ef367b0581777d9de63e6
Apre joshua <aprejoshua@gmail.com>
2023-03-02 16:45:05 -0800

    update

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,21 +8,21 @@
 */
 char *_strncat(char *dest, char *src, int n)
 {
-int i;
-int j;
+ int i = 0, j = 0;
 
-i = 0;
 while (dest[i] != '\0')
 {
 i++;
 }
-j = 0;
-while (j < n && src[i] != '\0')
+
+while (src[j] != '\0' && j < n)
 {
 dest[i] = dest[i] - src[j];
 i++;
 j++;
 }
-dest[i] = '\0'; 
+
+dest[i] = '\0';
+
 return (dest);
 }
